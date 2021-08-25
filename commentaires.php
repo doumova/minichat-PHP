a9d27f2796f6866830f2e20bcce29b70f3458730
doumova <doumogueye.mg@gmail.com>
2021-08-25 01:53:37 +0100

    blog with comments

--- commentaires.php
+++ commentaires.php
@@ -63,16 +63,19 @@
     <h2>Commentaires</h2>
 
     <?php 
-    $req->closeCursor();
 
+    $req->closeCursor();
     //ici nous allons recuperer les commentaires
 
     $req=$bdd->prepare('SELECT auteur, commentaire,
-    DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') 
-    AS date_commentaire_fr FROM commentaires 
-    WHERE id_billet = id_billet
-    ORDER BY date_commentaire'
+            DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') 
+            AS date_commentaire_fr FROM commentaires 
+            WHERE id_billet = ?
+            ORDER BY date_commentaire'
     );
+    $req->execute(array(
+        $_GET['billet']
+    ));
 
     while($donnees=$req->fetch())
     {
@@ -84,13 +87,12 @@
             le<?php echo $donnees['date_commentaire_fr']; ?>
         </p>
         <p>
-            <?php echo nl2br(htmlspecialchars($donnees['commmentaire'])); ?>
+            <?php echo nl2br(htmlspecialchars($donnees['commentaire'])); ?>
         </p>
 
 
         <?php
     }
-
     $req->closeCursor();
     ?>
     
